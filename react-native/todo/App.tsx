74483e8c1e088371425311fd63efbc22685bfe0b
jwAndroid <cjd9408er@naver.com>
2022-08-11 01:41:20 +0900

    Add project todo with redux-toolkit

---
+++ react-native/todo/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React, { memo } from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+export default memo(App);
